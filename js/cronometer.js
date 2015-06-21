efb4942272de5f293b78d9463ed2f9ccd8cc5e22
Rerisson Daniel <rerissondaniel@gmail.com>
2015-06-21 12:06:21 -0300

    Update cronometer.js

--- js/cronometer.js
+++ js/cronometer.js
@@ -2,7 +2,7 @@ var myVar = setInterval(function(){ myTimer() }, 1000);
 
 function myTimer() {
     var d = new Date();
-    var date = new Date(d.getYear(), d.getMonth(), 22 - d.getDate(), 23 - d.getHours(), 60 - d.getMinutes(), 60 - d.getSeconds(), 00);
+    var date = new Date(d.getYear(), d.getMonth(), 21 - d.getDate(), 23 - d.getHours(), 60 - d.getMinutes(), 60 - d.getSeconds(), 00);
 
     document.getElementById("day").textContent = '0' + date.getDate();
     document.getElementById("hour").textContent = date.getHours() >= 10 ? date.getHours() : '0' + date.getHours();
